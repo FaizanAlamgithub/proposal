21028f7fbc023709463a873a1e748ed38862d38a
Faizan Alam <mdfaizanalam019@gmail.com>
2025-03-13 01:57:15 +0530

    Adding files

--- client/src/pages/Proposal.jsx
+++ client/src/pages/Proposal.jsx
@@ -103,6 +103,91 @@
 
 // export default Proposal;
 
+// import React, { useEffect } from "react";
+
+// function Proposal({ proposal }) {
+//   const formatDate = (dateString) => {
+//     if (!dateString) return "Loading...";
+//     const date = new Date(dateString);
+//     return date.toLocaleDateString("en-US", {
+//       year: "numeric",
+//       month: "long",
+//       day: "numeric",
+//     });
+//   };
+
+//   return (
+//     <>
+//       <div className="box section-1">
+//         <div className="logo">
+//           <img src="../images/jca_logo.svg" alt="logo" />
+//         </div>
+//         {proposal ? (
+//           <div className="hero-content">
+//             <div className="midlle-text-container">
+//               <div className="proposal-text">
+//                 <p>Proposal</p>
+//               </div>
+//               <div className="propasal-hero">
+//                 <p>
+
+//                   {proposal ? proposal.proposalDescription : "Loading..."}
+//                   <span className="company-name">
+//                     {proposal ? proposal.companyName : "Loading..."}
+//                   </span>
+//                 </p>
+//               </div>
+//               <div className="proposal">
+//                 <p>Prepared For:</p>
+//                 <p className="client-name">
+//                   {proposal ? proposal.clientName : "Loading..."}
+//                 </p>
+//               </div>
+//             </div>
+//             <div className="hero-text-bottom">
+//               <div className="date-container">
+//                 <div className="hero-text-div">
+//                   <p className="issue-date">Issued On:</p>
+//                   <p className="date">
+//                     {proposal ? formatDate(proposal.createdDate) : "Loading..."}
+//                   </p>
+//                 </div>
+//                 <div className="hero-text-div">
+//                   <p className="issue-date">Expires On:</p>
+//                   <p className="date">
+//                     {proposal ? formatDate(proposal.expiryDate) : "Loading..."}
+//                   </p>
+//                 </div>
+//               </div>
+//               <div className="hero-text-address">
+//                 <p className="preparedBy">Prepared by:</p>
+//                 <p className="jordie-text">Jordie’s Creative Agency Pvt Ltd</p>
+//                 <p className="address">
+//                   #198, CMH Road, 2nd Floor, Desk No. 170 Indiranagar,
+//                   Bangalore, Karnataka, India 560038
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         ) : (
+//           <p>No proposal data available.</p>
+//         )}
+//         <div className="section-1-bottom-text">
+//           <p className="section-1-bottom-text-content">
+//             Confidentiality Statement: This document contains confidential and
+//             proprietary information belonging to Jordie’s Creative Agency. It is
+//             intended solely for the recipient and may not be shared, copied, or
+//             distributed without prior written consent. Any unauthorized use,
+//             reproduction, or disclosure is strictly prohibited.
+//           </p>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Proposal;
+
 import React, { useEffect } from "react";
 
 function Proposal({ proposal }) {
@@ -117,73 +202,69 @@ function Proposal({ proposal }) {
   };
 
   return (
-    <>
-      <div className="box section-1">
-        <div className="logo">
-          <img src="../images/jca_logo.svg" alt="logo" />
-        </div>
-        {proposal ? (
-          <div className="hero-content">
-            <div className="midlle-text-container">
-              <div className="proposal-text">
-                <p>Proposal</p>
-              </div>
-              <div className="propasal-hero">
-                <p>
-                  {/* A Strategic Approach to Building a Strong, Scalable, and
-                  Market-Leading Brand for */}
-                  {proposal ? proposal.proposalDescription : "Loading..."}
-                  <span className="company-name">
-                    {proposal ? proposal.companyName : "Loading..."}
-                  </span>
-                </p>
-              </div>
-              <div className="proposal">
-                <p>Prepared For:</p>
-                <p className="client-name">
-                  {proposal ? proposal.clientName : "Loading..."}
-                </p>
-              </div>
+    <section className="box section-1">
+      <div className="logo">
+        <img src="../images/jca_logo.svg" alt="logo" />
+      </div>
+      {proposal ? (
+        <div className="hero-content">
+          <div className="midlle-text-container">
+            <div className="proposal-text">
+              <p>Proposal</p>
             </div>
-            <div className="hero-text-bottom">
-              <div className="date-container">
-                <div className="hero-text-div">
-                  <p className="issue-date">Issued On:</p>
-                  <p className="date">
-                    {proposal ? formatDate(proposal.createdDate) : "Loading..."}
-                  </p>
-                </div>
-                <div className="hero-text-div">
-                  <p className="issue-date">Expires On:</p>
-                  <p className="date">
-                    {proposal ? formatDate(proposal.expiryDate) : "Loading..."}
-                  </p>
-                </div>
-              </div>
-              <div className="hero-text-address">
-                <p className="preparedBy">Prepared by:</p>
-                <p className="jordie-text">Jordie’s Creative Agency Pvt Ltd</p>
-                <p className="address">
-                  #198, CMH Road, 2nd Floor, Desk No. 170 Indiranagar,
-                  Bangalore, Karnataka, India 560038
-                </p>
-              </div>
+            <div className="propasal-hero">
+              <p>
+                {proposal ? proposal.proposalDescription : "Loading..."}
+                <span className="company-name">
+                  {proposal ? proposal.companyName : "Loading..."}
+                </span>
+              </p>
+            </div>
+            <div className="proposal">
+              <p>Prepared For:</p>
+              <p className="client-name">
+                {proposal ? proposal.clientName : "Loading..."}
+              </p>
             </div>
           </div>
-        ) : (
-          <p>No proposal data available.</p>
-        )}
-        <div className="section-1-bottom-text">
-          <p className="section-1-bottom-text-content">
-            Confidentiality Statement: This document contains confidential and
-            proprietary information belonging to Jordie’s Creative Agency. It is
-            intended solely for the recipient and may not be shared, copied, or
-            distributed without prior written consent. Any unauthorized use,
-            reproduction, or disclosure is strictly prohibited.
-          </p>
+          <div className="hero-text-bottom">
+            <div className="date-container">
+              <div className="hero-text-div">
+                <p className="issue-date">Issued On:</p>
+                <p className="date">
+                  {proposal ? formatDate(proposal.createdDate) : "Loading..."}
+                </p>
+              </div>
+              <div className="hero-text-div">
+                <p className="issue-date">Expires On:</p>
+                <p className="date">
+                  {proposal ? formatDate(proposal.expiryDate) : "Loading..."}
+                </p>
+              </div>
+            </div>
+            <div className="hero-text-address">
+              <p className="preparedBy">Prepared by:</p>
+              <p className="jordie-text">Jordie’s Creative Agency Pvt Ltd</p>
+              <p className="address">
+                #198, CMH Road, 2nd Floor, Desk No. 170 Indiranagar, Bangalore,
+                Karnataka, India 560038
+              </p>
+            </div>
+          </div>
         </div>
+      ) : (
+        <p>No proposal data available.</p>
+      )}
+      <div className="section-1-bottom-text">
+        <p className="section-1-bottom-text-content">
+          Confidentiality Statement: This document contains confidential and
+          proprietary information belonging to Jordie’s Creative Agency. It is
+          intended solely for the recipient and may not be shared, copied, or
+          distributed without prior written consent. Any unauthorized use,
+          reproduction, or disclosure is strictly prohibited.
+        </p>
       </div>
-    </>
+    </section>
   );
 }
 

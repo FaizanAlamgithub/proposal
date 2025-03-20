c1629d6e7a8fa8154fa61a4f7accf7f2dc88ee0e
md faizan alam <135551419+FaizanAlamgithub@users.noreply.github.com>
2025-03-21 05:03:08 +0530

    Update Dashboard.jsx

--- admin/src/components/Dashboard.jsx
+++ admin/src/components/Dashboard.jsx
@@ -593,7 +593,7 @@ function Dashboard({ downloadPDF }) {
       if (!token) throw new Error("Unauthorized access. Please log in.");
 
       const response = await fetch(
-        "http://localhost:5000/api/proposals/?admin=true",
+        "https://proposal-backend-1dom.onrender.com/api/proposals/?admin=true",
         {
           method: "GET",
           headers: {

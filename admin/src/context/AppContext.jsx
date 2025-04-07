0966990544c42de11a3fa03258c867ff3f703197
md faizan alam <135551419+FaizanAlamgithub@users.noreply.github.com>
2025-04-07 20:04:03 +0530

    Update AppContext.jsx

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -5,8 +5,7 @@ import { toast } from "react-toastify";
 export const AppContent = createContext();
 
 export const AppcontextProvider = (props) => {
-  // const backendUrl = "https://proposal-backend-tzo5.onrender.com";
-  const backendUrl = "http://13.60.13.12:5000";
+  const backendUrl = "http://localhost:5000";
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [adminData, setAdminData] = useState(false);

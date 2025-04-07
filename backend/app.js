403eb6dfc68fdc17d7602460feece606900c70a4
Faizan Alam <mdfaizanalam019@gmail.com>
2025-04-07 20:11:53 +0530

    Updating files

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 // const express = require("express");
 const connectDB = require("./src/config/db");
-const app = require("./src/app");
+const app = require("./src/index.js");
 
 connectDB();
 
